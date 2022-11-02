b1d1f2d5b2ca5657b46f9df5d9178995602cf967
Szymon Hachlowski <hachlowski.szymon@gmail.com>
2022-11-02 19:08:50 +0100

    add js (class add % remove)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
 'use strict';
+
+function titleClickHandler(event) {
+    const clickedElement = this;
+    console.log('Link was clicked!');
+
+    /* [DONE] remove class 'active' from all article links  */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+
+
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+
+    clickedElement.classList.add('active')
+    console.log('clickedElement:', clickedElement);
+
+
+    /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.post.active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+
+
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+}
